16a52cdabb90ee2b425fda88fd76b0e34aeda2a6
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-23 10:40:53 +0600

    added first commit

---
+++ src/components/Update.jsx
@@ -0,0 +1,31 @@
+import { useLoaderData } from "react-router-dom";
+
+const Update = () => {
+    const loadedUser = useLoaderData();
+
+    const handleUpdate = event => {
+        event.preventDefault()
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+
+        const user = { name, email }
+    }
+
+    return (
+        <div>
+            <h3>Updated information of : {loadedUser.name}</h3>
+
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" defaultValue={loadedUser?.name} id="" />
+                <br />
+                <input type="email" name="email" defaultValue={loadedUser?.email} id="" />
+                <br />
+                <input type="submit" value="Update" />
+            </form>
+        </div>
+    );
+};
+
+export default Update;
